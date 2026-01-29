bfccbed75fd50a88c2ac1e8c086d01712c376bb3
ShadowKnight-13 <padrago@yahoo.com>
2026-01-28 17:58:23 -0700

    1

--- cactus.gd
+++ cactus.gd
@@ -10,4 +10,4 @@ extends StaticBody2D
 func _on_hurt_box_body_shape_entered(_body_rid: RID, body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
 	if body.is_in_group("player"):
 		body.damage_player()
-		print("Ouch!")da
+		print("Ouch!")
