aa8eedf8c2bc6fe1dc7b6485981c7e9fab57b698
Dawson Robinson <rob21092@byui.edu>
2026-02-19 09:17:56 -0700

    level select menu added

--- UI/input_mapping_menu.gd
+++ UI/input_mapping_menu.gd
@@ -1,9 +1,12 @@
 extends CanvasLayer
 
+var listening_for
+var text_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	text_size = UiGlobals.text_size
+	set_font_size_recursive(self, text_size)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,5 +14,30 @@ func _process(delta: float) -> void:
 	pass
 
 
+func set_font_size_recursive(node: Node, size: int):
+	if node is Control:
+		node.add_theme_font_size_override("font_size", size)
+	
+	for child in node.get_children():
+		set_font_size_recursive(child, size)
+
+
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI/settings_menu.tscn")
+
+
+func _input(event):
+	if listening_for == "left" and event.pressed:
+		# Update InputMap
+		InputMap.action_erase_events("move_left")
+		InputMap.action_add_event("move_left", event)
+
+		# Update the UI text
+		$HBoxContainer/VBoxKeyboard/LeftKey.text = event.as_text()
+
+		# Stop listening
+		listening_for = ""
+
+
+func _on_left_key_pressed() -> void:
+	pass
--- level_select.gd
+++ level_select.gd
@@ -4,25 +4,18 @@ var text_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$VBoxBottom/StartButton.grab_focus()
 	text_size = UiGlobals.text_size
 	set_font_size_recursive(self, text_size)
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
 
 
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Main.tscn")
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/main_menu.tscn")
 
-
-func _on_settings_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://UI/settings_menu.tscn")
-
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit(0)
 
 func set_font_size_recursive(node: Node, size: int):
 	if node is Control:
@@ -30,7 +23,3 @@ func set_font_size_recursive(node: Node, size: int):
 	
 	for child in node.get_children():
 		set_font_size_recursive(child, size)
-
-
-func _on_level_select_pressed() -> void:
-	get_tree().change_scene_to_file("res://UI/level_select.tscn")
