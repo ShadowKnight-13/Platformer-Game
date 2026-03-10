d8f4bb6c451f63eff5d935a6fb2764ba3733fa26
WolfpackRadio <131834530+WolfpackRadio@users.noreply.github.com>
2026-03-10 08:16:06 -0700

    button works, added door

--- interactables/door.gd
+++ interactables/door.gd
@@ -1,1 +1,16 @@
 extends StaticBody2D
+
+@onready var interactable: Area2D = $interactable
+
+@export_file("*.tscn") var level: String = ""
+
+func _ready() -> void:
+	interactable.interacted.connect(_on_interact)
+
+
+func _on_interact() -> void:
+	if level == "":
+		push_warning("Door has no target_scene_path set")
+		return
+		
+	get_tree().change_scene_to_file(level)
