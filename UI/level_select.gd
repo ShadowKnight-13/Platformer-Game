4b2c734080e115ad76e0025777de8baeacd62f14
Dawson Robinson <rob21092@byui.edu>
2026-03-17 08:48:40 -0600

    test level button works

--- UI/level_select.gd
+++ UI/level_select.gd
@@ -62,4 +62,4 @@ func fade_out():
 func _on_test_level_pressed() -> void:
 	fade_out()
 	await get_tree().create_timer(1.0).timeout
-	get_tree().change_scene_to_file("res://Levels/Test Level.tscn")
+	get_tree().change_scene_to_file("res://Main_TestLevel.tscn")
